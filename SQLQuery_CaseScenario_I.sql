ee285ae2d3ead2151fe44433841747828f2bf385
AlphaSOJ <148491075+AlphaSoji@users.noreply.github.com>
2025-07-04 14:20:41 +0100

    Add files via upload

---
+++ SQLQuery_CaseScenario_I.sql
@@ -0,0 +1,124 @@
+SELECT * FROM KMS
+
+----Alter Table to change to 2 decimal places
+
+ALTER TABLE KMS
+ALTER COLUMN Sales DECIMAL (10,2)
+
+----a)	Which product category had the highest sales?
+
+SELECT TOP 1 Product_Category, MAX(Sales) AS Highest_Selling_Product
+FROM 
+	KMS
+GROUP BY 
+	Product_Category
+ORDER BY 
+	Product_Category Desc
+
+
+
+----b)	What are the Top 3 and Bottom 3 regions in terms of sales?
+----Top 3
+
+SELECT TOP 3 Region, MAX(Sales) AS Regional_Sales_Performance
+FROM KMS
+GROUP BY Region
+ORDER BY Region Asc
+
+----Botton 3
+
+SELECT TOP 3 Region, MIN(Sales) AS Regional_Sales_Performance
+FROM KMS
+GROUP BY Region
+ORDER BY Region Desc
+
+----c)	What were the total sales of appliances in Ontario?
+----Product Sales
+
+SELECT Product_Sub_Category, SUM(Sales) AS Product_Sales
+FROM KMS
+GROUP BY Product_Sub_Category
+
+----Province sales
+
+SELECT Province, SUM(Sales) AS Provincial_Sales
+FROM KMS
+GROUP BY Province
+
+----Ontario Appliances sales
+
+SELECT Product_Sub_Category, SUM(Sales) AS Ontario_Appliances_Sales
+FROM KMS
+WHERE Product_Sub_Category = 'Appliances' AND Province = 'Ontario'
+GROUP BY Product_Sub_Category
+
+----Alternative
+SELECT Province, SUM(Sales) AS Appliances_Sales
+FROM KMS
+WHERE Product_Sub_Category = 'Appliances' AND Province = 'Ontario'
+GROUP BY Product_Sub_Category, Province
+
+
+----d)	Advise the management of KMS on what to do to increase the revenue from the bottom 10 customers.
+----ALTER Decimals
+
+ALTER TABLE KMS
+ALTER COLUMN Discount DECIMAL (10,2)
+ALTER TABLE KMS
+ALTER COLUMN Product_Base_Margin DECIMAL (10,2)
+ALTER TABLE KMS
+ALTER COLUMN Unit_Price DECIMAL (10,2)
+ALTER TABLE KMS
+ALTER COLUMN Shipping_Cost DECIMAL (10,2)
+
+----Bottom 10 Customers
+
+SELECT TOP 10
+    Customer_Name,
+    SUM(Sales) AS total_customer_sales
+FROM
+    KMS
+GROUP BY
+    customer_name
+ORDER BY
+    total_customer_sales Asc
+
+
+----Extract relevant columns
+
+SELECT Row_ID, Order_Priority, Order_Quantity, Sales, Discount, Unit_Price, Shipping_Cost, Product_Base_Margin
+FROM KMS
+
+----Chech for trends
+----Order Quantity ----No correlation to Sales
+
+SELECT Row_ID, Order_Priority, Order_Quantity, Sales, Discount, Unit_Price, Shipping_Cost, Product_Base_Margin
+FROM KMS
+ORDER BY Order_Quantity Desc
+
+----Discount-- -N0 correlation to sales
+
+SELECT Row_ID, Order_Priority, Order_Quantity, Sales, Discount, Unit_Price, Shipping_Cost, Product_Base_Margin
+FROM KMS
+ORDER BY Discount Desc
+
+----Unit_Price
+
+SELECT Row_ID, Order_Priority, Order_Quantity, Sales, Discount, Unit_Price, Shipping_Cost, Product_Base_Margin
+FROM KMS
+ORDER BY Unit_Price Desc
+
+----e)	KMS incurred the most shipping cost using which shipping method?
+
+SELECT TOP 1
+    ship_mode,
+    SUM(shipping_cost) AS total_shipping_cost
+FROM
+    KMS
+GROUP BY
+    ship_mode
+ORDER BY
+    total_shipping_cost DESC
+
+
+SELECT * FROM KMS
